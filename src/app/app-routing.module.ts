3a214e5f01b0df62f5c82efcf5d382e844c73e32
mpumzee <mpumzcode@gmail.com>
2019-07-24 20:55:16 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "login", component: LoginComponent },
+  { path: "", redirectTo: "/login", pathMatch: "full" },
+  // {
+  //   path: "menu",
+  //   component: MenuComponent,
+  //   children: [
+  //     { path: "dashboard", component: DashboardComponent },
+  //     { path: "fuel", component: FuelComponent },
+  //     { path: "users", component: UsersComponent }
+  //   ],
+  //   runGuardsAndResolvers: "always"
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+export const routingComponents = [LoginComponent];
